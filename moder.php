36da72d90e69528dc543403e919b06124337233b
SergeAlekseev <serge.alekseev999@gmail.com>
2020-06-19 01:57:17 +0300

    3

--- moder.php
+++ moder.php
@@ -9,9 +9,7 @@ if (isset($_POST['del'])) {
   if (isset($_POST['idp'])) { $id_product = $_POST['idp']; if ($id_product == '') { unset($id_product);} } 
 
     $result1 = mysqli_query($db, "DELETE FROM products WHERE id_product = $id_product");
-    if($result1!='TRUE'){
-         exit($id_product);
-    }
+    
     session_start();
     $redicet = $_SERVER['HTTP_REFERER'];
         header("Location: $redicet");
